6888ce8c9c84eade8e30e9218367da838bb2e667
jerorpo <jrestrepofdez@gmail.com>
2026-09-15 23:45:48 -0500

    boton

---
+++ Feid/src/IniciarJuego.ts
@@ -0,0 +1,35 @@
+import * as ecs from '@8thwall/ecs'
+
+ecs.registerComponent({
+  name: 'IniciarJuego',
+
+  schema: {
+    mensaje: ecs.eid,
+    boton: ecs.eid,
+  },
+
+  stateMachine: ({world, eid, schemaAttribute}) => {
+    ecs.defineState('esperando')
+      .initial()
+      .listen(eid, ecs.input.SCREEN_TOUCH_START, () => {
+
+        const {mensaje, boton} = schemaAttribute.get(eid)
+
+        // Ocultar mensaje
+        ecs.Scale.set(world, mensaje, {
+          x: 0,
+          y: 0,
+          z: 0,
+        })
+
+        // Ocultar botón
+        ecs.Scale.set(world, boton, {
+          x: 0,
+          y: 0,
+          z: 0,
+        })
+
+        console.log('¡EMPEZÓ EL JUEGO!')
+      })
+  },
+})
