fed4cb0b516c026fc5715798a4c8dff8b2ede82d
jerorpo <jrestrepofdez@gmail.com>
2026-09-16 00:59:43 -0500

    d

--- Feid/src/ControlInicio.ts
+++ Feid/src/ControlInicio.ts
@@ -6,33 +6,52 @@ ecs.registerComponent({
   schema: {
     boton: ecs.eid,
     mensaje: ecs.eid,
+    escenario: ecs.eid,
+    personaje: ecs.eid,
   },
 
   stateMachine: ({world, eid, schemaAttribute}) => {
     ecs.defineState('esperando')
       .initial()
       .onEnter(() => {
-        const {boton} = schemaAttribute.get(eid)
 
+        const {escenario, personaje, boton} = schemaAttribute.get(eid)
+
+        // Ocultar escenario
+        ecs.Scale.set(world, escenario, {
+          x: 0,
+          y: 0,
+          z: 0,
+        })
+
+        // Ocultar personaje
+        ecs.Scale.set(world, personaje, {
+          x: 0,
+          y: 0,
+          z: 0,
+        })
+
+        // Escuchar el botón
         world.events.addListener(
           boton,
           ecs.input.UI_CLICK,
           () => {
+
             const {mensaje} = schemaAttribute.get(eid)
 
-            ecs.Scale.set(world, mensaje, {
-              x: 0,
-              y: 0,
-              z: 0,
+            // Ocultar el texto correctamente
+            ecs.Ui.set(world, mensaje, {
+              display: 'none',
             })
 
+            // Ocultar el botón
             ecs.Scale.set(world, boton, {
               x: 0,
               y: 0,
               z: 0,
             })
 
-            console.log('🔥 EMPEZAR PRESIONADO')
+            console.log('🔥 EMPEZÓ EL JUEGO')
           }
         )
       })
