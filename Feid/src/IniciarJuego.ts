232013990c98f529934638092c3d7fa6592eed91
jerorpo <jrestrepofdez@gmail.com>
2026-09-16 00:12:52 -0500

    e

--- Feid/src/IniciarJuego.ts
+++ Feid/src/IniciarJuego.ts
@@ -6,11 +6,32 @@ ecs.registerComponent({
   schema: {
     mensaje: ecs.eid,
     boton: ecs.eid,
+    escenario: ecs.eid,
+    personaje: ecs.eid,
   },
 
   stateMachine: ({world, eid, schemaAttribute}) => {
-    ecs.defineState('esperando')
+
+    ecs.defineState('inicio')
       .initial()
+      .onEnter(() => {
+
+        const {escenario, personaje} = schemaAttribute.get(eid)
+
+        // Ocultar escenario
+        ecs.Scale.set(world, escenario, {
+          x: 0,
+          y: 0,
+          z: 0,
+        })
+
+        // Ocultar personaje
+        ecs.Scale.set(world, personaje, {
+          x: 0,
+          y: 0,
+          z: 0,
+        })
+      })
       .listen(eid, ecs.input.SCREEN_TOUCH_START, () => {
 
         const {mensaje, boton} = schemaAttribute.get(eid)
@@ -29,7 +50,7 @@ ecs.registerComponent({
           z: 0,
         })
 
-        console.log('¡EMPEZÓ EL JUEGO!')
+        console.log('¡EMPEZÓ EL MINI-JUEGO!')
       })
   },
 })
