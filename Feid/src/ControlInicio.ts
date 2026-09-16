8a1287d65bf0e724a3d7d92a78b32b35b50542b3
jerorpo <jrestrepofdez@gmail.com>
2026-09-16 01:27:10 -0500

    e

--- Feid/src/ControlInicio.ts
+++ Feid/src/ControlInicio.ts
@@ -6,6 +6,7 @@ ecs.registerComponent({
   schema: {
     boton: ecs.eid,
     mensaje: ecs.eid,
+    miniJuego: ecs.eid,
   },
 
   stateMachine: ({world, eid, schemaAttribute}) => {
@@ -20,21 +21,30 @@ ecs.registerComponent({
           ecs.input.UI_CLICK,
           () => {
 
-            const {mensaje} = schemaAttribute.get(eid)
+            const {mensaje, miniJuego} = schemaAttribute.get(eid)
 
+            // Ocultar mensaje
             ecs.Scale.set(world, mensaje, {
               x: 0,
               y: 0,
               z: 0,
             })
 
+            // Ocultar botón
             ecs.Scale.set(world, boton, {
               x: 0,
               y: 0,
               z: 0,
             })
 
-            console.log('🔥 EMPEZAR FUNCIONÓ')
+            // Mostrar mini-juego
+            ecs.Scale.set(world, miniJuego, {
+              x: 1,
+              y: 1,
+              z: 1,
+            })
+
+            console.log('🔥 MINI-JUEGO ACTIVADO')
           }
         )
       })
